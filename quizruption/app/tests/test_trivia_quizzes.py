58fd62f338de3560f8533ff4d992c5c30113c766
Kara <klynch@rocketsoftware.com>
2025-11-13 13:10:32 -0600

    Added test trivia and personality quizzes

---
+++ quizruption/app/tests/test_trivia_quizzes.py
@@ -0,0 +1,161 @@
+quiz1 = {
+    "title": "Marvel Cinematic Universe Trivia",
+    "author": "QuizruptionBot",
+    "description": "Test your knowledge of the Marvel Cinematic Universe.",
+    "type": "trivia",
+    "questions": [
+        {
+            "text": "Who is the first Avenger in the MCU timeline?",
+            "answers": [
+                {"text": "Iron Man", "is_correct": False},
+                {"text": "Captain America", "is_correct": True},
+                {"text": "Thor", "is_correct": False},
+                {"text": "Hulk", "is_correct": False}
+            ]
+        },
+        {
+            "text": "Which Infinity Stone does Vision have?",
+            "answers": [
+                {"text": "Mind Stone", "is_correct": True},
+                {"text": "Time Stone", "is_correct": False},
+                {"text": "Space Stone", "is_correct": False},
+                {"text": "Reality Stone", "is_correct": False}
+            ]
+        },
+        {
+            "text": "Who directed 'Avengers: Endgame'?",
+            "answers": [
+                {"text": "Joss Whedon", "is_correct": False},
+                {"text": "Jon Favreau", "is_correct": False},
+                {"text": "Russo Brothers", "is_correct": True},
+                {"text": "Taika Waititi", "is_correct": False}
+            ]
+        },
+        {
+            "text": "What is Black Panther's real name?",
+            "answers": [
+                {"text": "T'Challa", "is_correct": True},
+                {"text": "M'Baku", "is_correct": False},
+                {"text": "Shuri", "is_correct": False},
+                {"text": "N'Jadaka", "is_correct": False}
+            ]
+        },
+        {
+            "text": "Which movie introduced Spider-Man to the MCU?",
+            "answers": [
+                {"text": "Spider-Man: Homecoming", "is_correct": False},
+                {"text": "Captain America: Civil War", "is_correct": True},
+                {"text": "Avengers: Infinity War", "is_correct": False},
+                {"text": "Doctor Strange", "is_correct": False}
+            ]
+        }
+    ]
+}
+
+quiz2 = {
+    "title": "Classic TV Shows Trivia",
+    "author": "QuizruptionBot",
+    "description": "How well do you know classic television?",
+    "type": "trivia",
+    "questions": [
+        {
+            "text": "Which sitcom features the characters Monica, Chandler, and Joey?",
+            "answers": [
+                {"text": "Friends", "is_correct": True},
+                {"text": "Seinfeld", "is_correct": False},
+                {"text": "The Office", "is_correct": False},
+                {"text": "Parks and Recreation", "is_correct": False}
+            ]
+        },
+        {
+            "text": "What is the name of the coffee shop in 'Friends'?",
+            "answers": [
+                {"text": "Central Perk", "is_correct": True},
+                {"text": "Monk's Café", "is_correct": False},
+                {"text": "Luke's Diner", "is_correct": False},
+                {"text": "The Max", "is_correct": False}
+            ]
+        },
+        {
+            "text": "Who is the boss in 'The Office' (US) for most seasons?",
+            "answers": [
+                {"text": "Michael Scott", "is_correct": True},
+                {"text": "Jim Halpert", "is_correct": False},
+                {"text": "Dwight Schrute", "is_correct": False},
+                {"text": "Pam Beesly", "is_correct": False}
+            ]
+        },
+        {
+            "text": "Which show is set in the fictional town of Springfield?",
+            "answers": [
+                {"text": "The Simpsons", "is_correct": True},
+                {"text": "Family Guy", "is_correct": False},
+                {"text": "South Park", "is_correct": False},
+                {"text": "Futurama", "is_correct": False}
+            ]
+        },
+        {
+            "text": "Who played the character of Elaine Benes?",
+            "answers": [
+                {"text": "Julia Louis-Dreyfus", "is_correct": True},
+                {"text": "Courteney Cox", "is_correct": False},
+                {"text": "Jennifer Aniston", "is_correct": False},
+                {"text": "Lisa Kudrow", "is_correct": False}
+            ]
+        }
+    ]
+}
+
+quiz3 = {
+    "title": "Music Legends Trivia",
+    "author": "QuizruptionBot",
+    "description": "Trivia about famous music legends.",
+    "type": "trivia",
+    "questions": [
+        {
+            "text": "Who is known as the 'King of Pop'?",
+            "answers": [
+                {"text": "Elvis Presley", "is_correct": False},
+                {"text": "Michael Jackson", "is_correct": True},
+                {"text": "Prince", "is_correct": False},
+                {"text": "Madonna", "is_correct": False}
+            ]
+        },
+        {
+            "text": "Which band released the album 'Abbey Road'?",
+            "answers": [
+                {"text": "The Beatles", "is_correct": True},
+                {"text": "The Rolling Stones", "is_correct": False},
+                {"text": "Queen", "is_correct": False},
+                {"text": "Pink Floyd", "is_correct": False}
+            ]
+        },
+        {
+            "text": "Who sang 'Like a Virgin'?",
+            "answers": [
+                {"text": "Madonna", "is_correct": True},
+                {"text": "Whitney Houston", "is_correct": False},
+                {"text": "Lady Gaga", "is_correct": False},
+                {"text": "Cyndi Lauper", "is_correct": False}
+            ]
+        },
+        {
+            "text": "Which rapper is known for the album 'The Marshall Mathers LP'?",
+            "answers": [
+                {"text": "Jay-Z", "is_correct": False},
+                {"text": "Eminem", "is_correct": True},
+                {"text": "Kanye West", "is_correct": False},
+                {"text": "Dr. Dre", "is_correct": False}
+            ]
+        },
+        {
+            "text": "Who is the lead singer of U2?",
+            "answers": [
+                {"text": "Bono", "is_correct": True},
+                {"text": "Sting", "is_correct": False},
+                {"text": "Freddie Mercury", "is_correct": False},
+                {"text": "Bruce Springsteen", "is_correct": False}
+            ]
+        }
+    ]
+}
